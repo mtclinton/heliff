db295ccaa2276d9d5c55a1e0aaf0a2a35965bcb2
mtclinton <maxtclinton@gmail.com>
2021-08-13 21:19:20 -0400

    adding twig template functionality

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function home()
+    {
+        return $this->render('home.html.twig', [
+
+        ]);
+
+    }
+
+    /**
+     * @Route("/test")
+     */
+    public function show()
+    {
+        return new Response('Future page to show a test!');
+    }
+
+    /**
+     * @Route("/content/{slug}")
+     */
+    public function content($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show the content "%s"!',
+            $slug
+        ));
+    }
+}
